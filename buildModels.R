fdbac31bf478544dd448aa8c89da224546bb206a
Rob Gordon <rfgordonjr@gmail.com>
2019-11-06 19:49:46 -0500

    Created functions for updating BLUP over time.

--- buildModels.R
+++ buildModels.R
@@ -332,37 +332,43 @@ first3Years <- test %>%
   ungroup() %>% 
   filter(rowNum <= 3) 
 ## how many years did each play?
-test %>% 
+careerLengths <- test %>% 
   filter(playerID %in% first3Years$playerID) %>% 
   group_by(playerID) %>% 
   summarise(maxYearService = max(yearService)) %>% 
   ungroup() 
+careerLengths
+
+# merge first 3 years with career lengths to know how long loops should be
+first3 <- first3Years %>% left_join(careerLengths, by = "playerID")
+
+
 
 # estYearlyBA <- function(data, object, ID, yearsToPred){
-  data = first3Years; object = fit2; ID = "andruel01"; yearsToPred = 10
-  v <- VarCorr(object);
-  G <- as.matrix(Matrix::bdiag(v))
-  redData <- data %>% filter(playerID == ID)
-  Z = matrix(cbind(rep(1, yearsToPred), 1:yearsToPred), ncol = 2)
-  # Var(y) = V = ZGZ' + R where Var(\gamma) = G and Var(\epsilon) = R
-  V = Z%*%G%*%t(Z) + sigma(fit2)*diag(yearsToPred)
-  ## \hat{\beta} = (X'\hat{V}^{-1}X)^-X'\hat{V}^{-1}y 
-  X = data %>% 
-    filter(playerID == ID) %>% 
-    mutate(ones = 1) %>% 
-    select(ones, GS, soPer, yearService) %>% 
-    as.matrix()
-  rows <- as.numeric(data %>% filter(playerID == ID) %>% nrow())
-  Z_red = matrix(cbind(rep(1, rows), 1:rows), ncol = 2)
-  V_red = Z_red%*%G%*%t(Z_red) + sigma(fit2)*diag(rows)
-  y_red = data %>% filter(playerID == ID) %>% select(batAvg)
-  # beta_hat = solve(t(X)%*%solve(V_red)%*%X)%*%t(X)%*%solve(V_red)%*%y_red$batAvg
-  beta_hat <- fixef(fit2)
-  ## \hat{\gamma} = \hat{G}Z'\hat{V}^{-1}(y - X\hat{\beta})
-  gamma_hat = G%*%t(Z_red)%*%solve(V_red)%*%(y_red$batAvg - X%*%beta_hat)
-  preds = X%*%beta_hat + Z%*%gamma_hat
-  return(preds)
-}
+#   data = first3Years; object = fit2; ID = "andruel01"; yearsToPred = 10
+#   v <- VarCorr(object);
+#   G <- as.matrix(Matrix::bdiag(v))
+#   redData <- data %>% filter(playerID == ID)
+#   Z = matrix(cbind(rep(1, yearsToPred), 1:yearsToPred), ncol = 2)
+#   # Var(y) = V = ZGZ' + R where Var(\gamma) = G and Var(\epsilon) = R
+#   V = Z%*%G%*%t(Z) + sigma(fit2)*diag(yearsToPred)
+#   ## \hat{\beta} = (X'\hat{V}^{-1}X)^-X'\hat{V}^{-1}y 
+#   X = data %>% 
+#     filter(playerID == ID) %>% 
+#     mutate(ones = 1) %>% 
+#     select(ones, GS, soPer, yearService) %>% 
+#     as.matrix()
+#   rows <- as.numeric(data %>% filter(playerID == ID) %>% nrow())
+#   Z_red = matrix(cbind(rep(1, rows), 1:rows), ncol = 2)
+#   V_red = Z_red%*%G%*%t(Z_red) + sigma(fit2)*diag(rows)
+#   y_red = data %>% filter(playerID == ID) %>% select(batAvg)
+#   # beta_hat = solve(t(X)%*%solve(V_red)%*%X)%*%t(X)%*%solve(V_red)%*%y_red$batAvg
+#   beta_hat <- fixef(fit2)
+#   ## \hat{\gamma} = \hat{G}Z'\hat{V}^{-1}(y - X\hat{\beta})
+#   gamma_hat = G%*%t(Z_red)%*%solve(V_red)%*%(y_red$batAvg - X%*%beta_hat)
+#   preds = X%*%beta_hat + Z%*%gamma_hat
+#   return(preds)
+# }
 # estYearlyBA(data = first3Years, object = fit2, ID = "andruel01", yearsToPred = 10)
 
 ## Build custom BLUP on Test Set subjects
@@ -375,10 +381,12 @@ allSubPredParMC <- function(obj, data, y, cores){
   # data: data.frame
   # y: character
   # cores: integer (use detectCores to figure out what is available)
-  obj = fit2;data = first3Years;y = "batAvg";cores = 4
+  # obj = fit2;data = first3Years;y = "batAvg";cores = 4
+  # obj = fit3;data = first3_simple;y = "batAvg";cores = 4
   
   getSubPred <- function(obj, playerID, data, y){
     # obj = fit2; playerID = "andruel01"; data = first3Years; y = "batAvg"
+    # playerID = ids[1]
     D <- matrix(c(VarCorr(obj))$playerID[1:2, 1:2], 2, 2) # change 2 to number of random effects
     len <- sum(ifelse(data$playerID==playerID, 1, 0))
     onlyplayerID <- data[which(data$playerID==playerID),]
@@ -400,6 +408,7 @@ allSubPredParMC <- function(obj, data, y, cores){
   temp <- plyr::ldply(mclapply(X=ids, FUN=getSubPred, obj=obj, data=data, y=y, mc.cores=cores), data.frame)
   
   # Also return random effects parameters to returned data frame
+  # note: if taken from never-before seen players, will return Nulls
   ranefs <- ranef(obj)[[1]]
   names(ranefs) <- c("ranInterceptTrue", "ranSlopeTrue")
   ranefs$playerID <- rownames(ranefs); rownames(ranefs) <- c(1:nrow(ranefs))
@@ -407,79 +416,204 @@ allSubPredParMC <- function(obj, data, y, cores){
   temp2 <- dplyr::left_join(temp, ranefs, by="playerID")
   return(temp2)
 }
-allSubPredParMC(obj = fit2,data = first3Years,y = "batAvg",cores = 4) 
+first3_blups <- allSubPredParMC(obj = fit2,data = first3,y = "batAvg",cores = 4) 
 
+## Demonstrate power of blup ####
+# First refit model with only time and blups
+fit3 <- lmer(batAvg ~ 1
+             + yearService
+             + (1 + yearService | playerID), 
+             data = train)
+summary(fit3)
+head(lme4::ranef(fit3)[[1]])
+lme4::fixef(fit3)
+
+train$pred3 <- predict(fit3, train, re.form = NULL)
+test$pred3 <- predict(fit3, test, allow.new.levels = TRUE)
+testWorst6_3 <- test %>% 
+  group_by(playerID) %>% 
+  mutate(sqError = (batAvg - pred3)^2) %>% 
+  summarise(rmse = sqrt(mean(sqError, na.rm=TRUE))) %>% 
+  ungroup() %>% 
+  arrange(desc(rmse)) %>% 
+  slice(1:6) 
+plotTestWorst3 <- test %>% 
+  # filter(playerID %in% trainWorst6$playerID) %>% 
+  inner_join(testWorst6_3, by = "playerID") %>% 
+  mutate(customLabel = paste0(nameFirst, " ", nameLast, ": RMSE = ", round(rmse, 4))) %>% 
+  select(playerID, customLabel, yearID, actual = batAvg, predicted = pred3) %>% 
+  gather(var, value, -c(playerID, customLabel, yearID)) %>% 
+  ggplot(.) +
+  geom_line(aes(x = yearID, y = value, group = var, col = var)) +
+  geom_point(aes(x = yearID, y = value, group = var, col = var)) +
+  facet_wrap(~customLabel) +
+  scale_y_continuous(labels=scaleFUNy) +
+  labs(title = "Predicted vs Actual Plots by year for worst-fitting players (Test set)",
+       subtitle = "RMSE = Root Mean Squared Error",
+       x = "Year Played",
+       y = "Batting Average")
+plotTestWorst3
+ggsave("plotTestWorst3.png", plot = plotTestWorst3, path = here::here('plots'))
+
+# testBest6_3 <- test %>% 
+#   group_by(playerID) %>% 
+#   mutate(sqError = (batAvg - pred3)^2) %>% 
+#   summarise(rmse = sqrt(mean(sqError, na.rm=TRUE))) %>% 
+#   ungroup() %>% 
+#   arrange(rmse) %>% 
+#   slice(1:6) 
+# test %>% 
+#   # filter(playerID %in% trainWorst6$playerID) %>% 
+#   inner_join(testBest6_3, by = "playerID") %>% 
+#   mutate(customLabel = paste0(nameFirst, " ", nameLast, ": RMSE = ", round(rmse, 4))) %>% 
+#   select(playerID, customLabel, yearID, actual = batAvg, predicted = pred3) %>% 
+#   gather(var, value, -c(playerID, customLabel, yearID)) %>% 
+#   ggplot(.) +
+#   geom_line(aes(x = yearID, y = value, group = var, col = var)) +
+#   geom_point(aes(x = yearID, y = value, group = var, col = var)) +
+#   facet_wrap(~customLabel) +
+#   scale_y_continuous(labels=scaleFUNy) +
+#   labs(title = "Predicted vs Actual Plots by year for best-fitting players (Test set)",
+#        subtitle = "RMSE = Root Mean Squared Error",
+#        x = "Year Played",
+#        y = "Batting Average")
+
+## These results are very rudimentary. 
+## Let's recalculate the blup for each day in the future to get better estimates
 # Demonstrate power of blup
-getSubPredRed3 <- function(obj, playerID, dat, y){
-  obj = fit2; playerID=ids[1]; dat = first3Years; y = "batAvg"
-  require('lme4')
-  onlyplayerID <- dat[which(dat$playerID==playerID),]
-  D <- matrix(c(VarCorr(obj))$playerID[1:2, 1:2], 2, 2) # adjust "2" for num of ranefs
-  len <- sum(ifelse(dat$playerID==playerID, 1, 0))
-  Zt <- matrix(data=rbind(c(rep(1, len)), seq(min(onlyplayerID$yearService)
-                                              ,max(onlyplayerID$yearService)
-                                              ,1))
-               , 2, len)
-  Z <- t(Zt)
-  
-  temp <- onlyplayerID
-  ZtRed <- matrix(data=rbind(c(rep(1,1)), seq(max(onlyplayerID$yearService)-1,
-                                              max(onlyplayerID$yearService)-1, 1)),
-                  2, 1)
-  # ZtRed <- matrix(data=rbind(c(rep(1,1)), seq(1, 
-  #                                             max(onlyplayerID$yearService), 
-  #                                             1)
-  #                            ),
-  #                 nrow = 2)
-  ZRed <- t(ZtRed)
-  SigmaRed <- diag(attr(VarCorr(obj), "sc")^2, 1, 1)
-  Sigma <- diag(attr(VarCorr(obj), "sc")^2, len, len)
-  # temp$predPop <- (fixef(obj))[names(fixef(obj)) %in% c("(Intercept)")] +
-  #   (fixef(obj))[names(fixef(obj)) %in% c("x1")]
-  temp$predPop <- (fixef(obj))[names(fixef(obj)) %in% c("(Intercept)")] + (fixef(obj))[names(fixef(obj)) %in% c("GS")] + (fixef(obj))[names(fixef(obj)) %in% c("soPer")] + (fixef(obj))[names(fixef(obj)) %in% c("yearService")]
-  # uhatRed <- D%*%ZtRed%*%solve(ZRed%*%D%*%ZtRed + SigmaRed)%*%as.matrix(temp[,names(temp) %in% y]-temp$predPop[1:1])  
-  uhatRed <- D%*%ZtRed%*%solve(ZRed%*%D%*%ZtRed + SigmaRed)%*%as.matrix(temp[,names(temp) %in% y]-temp$predPop)  
-  uhat <- D%*%Zt%*%solve(Z%*%D%*%Zt + Sigma)%*%as.matrix(temp[,names(temp) %in% y]-temp$predPop[1:1])  
-  temp$predSubjectCustomRed <- as.numeric(temp$predPop + Z%*%uhatRed)
-  
-  temp$numObs <- 1
-  temp$predNeeded <- len
-  temp <- temp[which(temp$timePoint==len), names(temp) %in% c("playerID", "yearService"
-                                                              , "predSubjectCustomeRed", "predPopulation", y, "timePoint", "numObs", "predNeeded")]
-  if(len>1){
-    for(i in 2:(len-1)){
-      temp2 <- onlyplayerID
-      ZRed <- matrix(data=c(c(rep(1, i-1+1))
-                            , seq(max(onlyplayerID$yearService)-i
-                                  ,max(onlyplayerID$yearService)-1, 1))
-                     ,ncol=2, nrow=i-1+1)
-      ZtRed <- t(ZRed)
-      SigmaRed <- diag(attr(VarCorr(obj), "sc")^2, i-1+1, i-1+1)
-      temp2$predPop <- (fixef(obj))[names(fixef) %in% c("(Intercept)")] +
-        (fixef(obj))[names(fixef) %in% c("x1")]
-      uhatRed <- D%*%ZtRed%*%solve(ZRed%*%D%*%ZtRed + SigmaRed)%*%as.matrix(temp2[c(1:(i-1+1))
-                                                                                  ,names(temp2) %in% y] - temp2$predPop[1:(i-1+1)])
-      
-      temp2$predSubjectCustomRed <- as.numeric(temp2$predPop + Z%*%uhatRed)
-      temp2$numObs <- i-1+1
-      temp2$predNeeded <- len
-      
-      temp2 <- temp2[which(temp2$timePoint==len),
-                     names(temp2) %in% c("playerID", "yearService", "predSubjectCustomRed",
-                                         "predPopulation", y, "timePoint", "numObs", "predNeeded")] 
-      temp <- rbind(temp, temp2)      
-    }
-  }
-  return(temp)
-}
 
-allSubPredRed3 <- function(obj, dat, y){
-  obj = fit2;dat=first3Years;y = "batAvg"
-  ids <- unique(dat$playerID)
-  temp<- getSubPredRed3(obj, playerID=ids[1], dat, y)
-  for(i in 2:length(ids)){
-    temp <- rbind(temp, getSubPredRed3(obj, playerID=ids[i], dat, y))
+## Once again start by calculating the blups for the first 3 years of the 6 worst players (in terms of their RMSE)
+first3_simple <- test %>% 
+  # filter(playerID %in% trainWorst6$playerID) %>% 
+  inner_join(testWorst6_3, by = "playerID") %>% 
+  group_by(playerID) %>% 
+  mutate(rowNum = row_number()) %>% 
+  ungroup() %>% 
+  filter(rowNum <= 3) 
+
+## build random effects parameters on this data set:
+# first3_blups <- allSubPredParMC(obj = fit2,data = first3,y = "batAvg",cores = 4) 
+allSubPredParMC3 <- function(obj, data, y, cores){
+  require('lme4')
+  require('parallel')
+  require('plyr')
+  
+  # obj: lme4 object (merMod)
+  # data: data.frame
+  # y: character
+  # cores: integer (use detectCores to figure out what is available)
+  # obj = fit2;data = first3Years;y = "batAvg";cores = 4
+  # obj = fit3;data = first3_simple;y = "batAvg";cores = 4
+  
+  getSubPred <- function(obj, playerID, data, y){
+    # obj = fit2; playerID = "andruel01"; data = first3Years; y = "batAvg"
+    # playerID = ids[1]
+    D <- matrix(c(VarCorr(obj))$playerID[1:2, 1:2], 2, 2) # change 2 to number of random effects
+    len <- sum(ifelse(data$playerID==playerID, 1, 0))
+    onlyplayerID <- data[which(data$playerID==playerID),]
+    Zt <- matrix(data=rbind(c(rep(1, len))
+                            , seq(min(onlyplayerID$yearService)
+                                  , max(onlyplayerID$yearService), 1))
+                 , 2, len)
+    Z <- t(Zt)
+    Sigma <- diag(attr(VarCorr(obj), "sc")^2, len, len)
+    onlyplayerID$predPop <- (fixef(obj))[names(fixef(obj)) %in% c("(Intercept)")] +  (fixef(obj))[names(fixef(obj)) %in% c("yearService")]
+    uhat <- D%*%Zt%*%solve(Z%*%D%*%Zt + Sigma)%*%as.matrix(onlyplayerID[,names(onlyplayerID) %in% y] - onlyplayerID$predPop)    
+    onlyplayerID$predSubjectCustom <- as.numeric(onlyplayerID$predPop + Z%*%uhat)
+    onlyplayerID$ranIntercept <- as.numeric(uhat[1])
+    onlyplayerID$ranSlope <- as.numeric(uhat[2])
+    return(onlyplayerID)
   }
-  return(temp)
+  ids <- unique(data$playerID)
+  # temp <- ldplyr(mclapply(X=ids, FUN=getSubPred, obj=obj, data=data, y=y, mc.cores=cores), data.frame)
+  temp <- plyr::ldply(mclapply(X=ids, FUN=getSubPred, obj=obj, data=data, y=y, mc.cores=cores), data.frame)
+  
+  # Also return random effects parameters to returned data frame
+  # note: if taken from never-before seen players, will return Nulls
+  ranefs <- ranef(obj)[[1]]
+  names(ranefs) <- c("ranInterceptTrue", "ranSlopeTrue")
+  ranefs$playerID <- rownames(ranefs); rownames(ranefs) <- c(1:nrow(ranefs))
+  require('dplyr')
+  temp2 <- dplyr::left_join(temp, ranefs, by="playerID")
+  
+  ## add fixed slope and intercept
+  fixefs <- fixef(obj)
+  # names(fixefs) <- c("fixedIntercept", "fixedSlope")
+  temp2 %>% 
+    mutate(fixedIntercept = fixefs[1],
+           fixedSlope = fixefs[2])
+  # return(temp2)
 }
-allSubPredRed3(obj = fit2,dat=first3Years,y = "batAvg")
+first3_simple_blups <- allSubPredParMC3(obj = fit3,data = first3_simple,y = "batAvg",cores = 4) 
+
+## Now we predict the next 5 years by increasing the yearService by 1 and recalculating the blup each iteration
+updateWithBlup <- function(id, data, obj, y, years){
+  # id = playerID from dataset
+  # data = name of data set created from allSubPredParMC3 function
+  # obj = object created from lme4::lmer
+  # y = response name
+  # years = number of predictions into future needed
+  ## test
+  # id = "escobal02"; data = first3_simple_blups; obj = fit3; y = "batAvg"; years = 5
+  library(dplyr)
+  library(lme4)
+  datared <- data %>% 
+    filter(playerID==id) %>% 
+    select(playerID, yearID, yearService, pred3, one_of(y), predPop, predSubjectCustom, fixedIntercept, ranIntercept, fixedSlope, ranSlope) %>% 
+    mutate(history = TRUE)
+  firstRowCalc <- nrow(datared)
+  for(i in 1:years){
+    
+    Zt = matrix(data=rbind(c(rep(1, max(datared$yearService)+1)), 
+                          seq(1, max(datared$yearService)+1, 1)
+                          )
+               , 2, max(datared$yearService)+1)
+    Z = t(Zt)
+    D <- matrix(c(VarCorr(obj))$playerID[1:2, 1:2], 2, 2)
+    Sigma <- diag(attr(VarCorr(obj), "sc")^2, max(datared$yearService)+1, max(datared$yearService)+1)
+    X = Z
+    V = Z%*%D%*%t(Z) + Sigma
+    
+    newRow <- datared %>% slice(nrow(.))
+    newRow$yearID = newRow$yearID + 1
+    newRow$yearService = newRow$yearService + 1
+    newRow$batAvg = newRow$fixedIntercept + newRow$ranIntercept + (newRow$fixedSlope + newRow$ranSlope)*newRow$yearService
+    beta = solve(t(X)%*%solve(V)%*%X)%*%t(X)%*%solve(V)%*%as.matrix(c(datared$batAvg, newRow$batAvg))
+    newRow$fixedIntercept <- beta[1,1]
+    newRow$fixedSlope <- beta[2,1]
+    newRow$predPop = newRow$fixedIntercept + newRow$fixedSlope*newRow$yearService
+    newRow$history = FALSE
+    datared <- rbind(datared, newRow)
+    
+    uhat = D%*%Zt%*%solve(Z%*%D%*%Zt + Sigma)%*%as.matrix(datared$batAvg - datared$predPop)
+    datared$ranIntercept[i + firstRowCalc] <- uhat[1,1]
+    datared$ranSlope[i + firstRowCalc] <- uhat[2,1]
+    datared$predSubjectCustom[i + firstRowCalc] = datared$predPop[i + firstRowCalc] + uhat[1,1] + uhat[2,1]*datared$yearService[i + firstRowCalc]
+    cat("i = ", i, ".\n",sep = "")
+    print(datared)
+    
+    
+  }
+  return(datared)
+}
+escobal02_preds <- updateWithBlup(id = "escobal02", data = first3_simple_blups, obj = fit3, y = "batAvg", years = 5)
+## plot preds vs actuals for this player
+escobal02_3 <- test %>%
+  inner_join(testWorst6_3, by = "playerID") %>%
+  filter(playerID %in% c("escobal02")) %>% 
+  left_join(escobal02_preds %>% filter(yearService>3) %>% select(newBatAvg = batAvg, everything()), by = c("yearID", "playerID", "yearService")) %>% 
+  mutate(customLabel = paste0(nameFirst, " ", nameLast, ": RMSE = ", round(rmse, 4))) %>% 
+  select(playerID, customLabel, yearID, actual = batAvg, predictedFixed = pred3.x, predictedBLUP = newBatAvg) %>% 
+  gather(var, value, -c(playerID, customLabel, yearID)) %>% 
+  ggplot(.) +
+  geom_line(aes(x = yearID, y = value, group = var, col = var)) +
+  geom_point(aes(x = yearID, y = value, group = var, col = var)) +
+  facet_wrap(~customLabel) +
+  scale_x_continuous(labels=scaleFUN) +
+  scale_y_continuous(labels=scaleFUNy) +
+  labs(title = "Predicted vs Actual Plots by year for worst-fitting players (Test set)",
+       subtitle = "First 3 years used for Green Prediction",
+       x = "Year Played",
+       y = "Batting Average")
+escobal02_3
+ggsave(filename = "escobal02_3.png",plot = escobal02_3,path = here::here('plots'))
